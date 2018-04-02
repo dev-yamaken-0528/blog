4e490e07287ff806350361922268f307b132eec6
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-04-02 12:39:28 +0900

    mod

--- themes/mytheme01/static/js/main.js
+++
@@ -1,7 +0,0 @@
-var items = document.getElementsByClassName("box");
-for( var i = 0; i < items.length; i++ ) {
-    //items[i].onclick = function () {
-    items[i].onmouseover = function () {
-      this.classList.toggle('anim');
-    }
-}
