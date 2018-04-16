e51ba3f0d19bc9ff6de461401d60015ad7fca78a
dev-yamaken-0528 <dev.yamaken.0528@gmail.com>
2018-04-16 18:34:33 +0900

    add post

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,1 @@
-//var items = document.getElementsByClassName("box");
-//for( var i = 0; i < items.length; i++ ) {
-//    //items[i].onclick = function () {
-//    items[i].onmouseover = function () {
-//      this.classList.toggle('anim');
-//    }
-//}
 
-$(document).ready(function(){
-  $(".dropdown-trigger").dropdown();
-})
